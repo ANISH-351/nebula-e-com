d426c5d59fb827b18646cc860057a2e31eee5332
Anish <anishani351@gmail.com>
2026-05-24 19:42:19 +0530

    fix import case sensitivity

--- client/src/components/home/Addsection.jsx
+++ client/src/components/home/Addsection.jsx
@@ -3,7 +3,7 @@ import add from "../../assets/home-images/add.png";
 
 function Addsection() {
   return (
-    <section className=" container mx-auto bg-white pb-8 px-4 sm:px-6 lg:px-10">
+    <section className=" container mx-auto bg-white  px-4 sm:px-6 lg:px-10">
       <div className="relative w-full h-[25vh] sm:h-[35vh] lg:h-[80vh] overflow-hidden rounded-3xl shadow-md">
         
         {/* Banner Image */}
